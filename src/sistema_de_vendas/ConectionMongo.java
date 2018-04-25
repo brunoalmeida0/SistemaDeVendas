88f7316c20613e645417b88e59fe796b53fa2ed2
Bruno Almeida <brunoalmeida356@gmail.com>
2018-04-24 21:34:52 -0300

    interface e banco de dados

---
+++ src/sistema_de_vendas/ConectionMongo.java
@@ -0,0 +1,37 @@
+package sistema_de_vendas;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConectionMongo {
+    
+    private DB db;
+    private DBCollection tabelaClientes;
+    private DBCollection tabelaProdutos;
+    
+    public void startMongo(){
+        try {
+
+                Mongo mongo = new Mongo("LocalHost", 27017);
+                db = mongo.getDB("sistemavendas");
+                tabelaClientes = db.getCollection("customers");
+                tabelaProdutos = db.getCollection("produtos");
+
+            } catch (UnknownHostException ex) {
+                Logger.getLogger(ConectionMongo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    
+    public DBCollection returnTableClientes(){
+        return tabelaClientes;
+    }
+    
+    public DBCollection returnTableProdutos(){
+        return tabelaProdutos;
+    }
+    
+}
